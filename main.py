17a4c55338e7fe2bc3041d1c549f277257009200
mariobigai <mariobigai1@gmail.com>
2023-02-08 17:34:40 -0300

    Extraindo o valor de um parâmetro dinâmicamente

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-url_base = url[0:19]
-print(url_base)
-
-url_parametros = url[20:36]
+#Separando a base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:] # indice_interrogacao + 1 para pular o '?'
 print(url_parametros)
+
+#Extraindo os valores dos parâmetros
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1 # +1 para pular o '='
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
